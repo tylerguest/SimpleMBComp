d788b242ed38a2647a9ebe6d8c16ecf6d03959c6
Tyler Guest <83128087+tylerguest@users.noreply.github.com>
2024-01-07 10:40:30 -0500

    Dialed in custom band control colors

--- Source/GUI/LookAndFeel.cpp
+++ Source/GUI/LookAndFeel.cpp
@@ -71,7 +71,7 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g,
 }
 
 void LookAndFeel::drawToggleButton(juce::Graphics& g,
-    juce::ToggleButton& toggleButton,
+    juce:: ToggleButton& toggleButton,
     bool shouldDrawButtonAsHighlighted,
     bool shouldDrawButtonAsDown)
 {
@@ -129,7 +129,10 @@ void LookAndFeel::drawToggleButton(juce::Graphics& g,
 
         const int cornerSize = 4;
 
-        g.setColour(buttonIsOn ? juce::Colours::white : juce::Colours::black);
+        g.setColour(buttonIsOn ?
+                    toggleButton.findColour(TextButton::ColourIds::buttonOnColourId) :
+                    toggleButton.findColour(TextButton::ColourIds::buttonColourId));
+
         g.fillRoundedRectangle(bounds.toFloat(), cornerSize);
 
         g.setColour(buttonIsOn ? juce::Colours::black : juce::Colours::white);
